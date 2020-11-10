96aa01ed6f8726d24cc08c2f2fb910804e5fc132
Antonín Vondrovic <antonin.vondrovic@educanet.cz>
2020-11-10 15:07:46 +0100

    added basic dumb proofing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String spacer = "-----------------------------------------------------";
         int currentTasks = 0;
-
         String[] stringTaskArray = new String[10];
+
+        // Replaced all of these with nothing so it won't say null if there is nothing written to them.
         stringTaskArray[0] = "";
         stringTaskArray[1] = "";
         stringTaskArray[2] = "";
@@ -30,13 +31,14 @@ public class Main {
                 "                                       | |   | |    \n" +
                 "                                       |_|   |_|    ");
         while (true) {
-            System.out.println(spacer + "\nYou can add up to 10 tasks!\n" + "You have " + currentTasks + "/10!\n" + spacer + "\n1) Manage tasks \n" + "2) View tasks\n" +"3) Exit");
+            System.out.println(spacer + "\nYou can add up to 10 tasks!\n" + "You have " + currentTasks + "/10!\n" + spacer + "\n1) Manage tasks \n" + "2) View tasks\n" + "3) Exit");
             while (true) {
-                int menuInput = sc.nextInt();
-                if (menuInput == 1) {
+                int menuInput = sc.nextInt(); //User input in the main menu
+
+                if (menuInput == 1) { //1 - Manage tasks
                     System.out.println(spacer + "\nTasks management\n" + spacer + "\n1) Create a new task\n2) Update a task\n3) Destroy a task\n4) Back to main menu");
-                    break;
-                } else if (menuInput == 2) {
+                    break; // Breaks the while loop and goes to the other while loop.
+                } else if (menuInput == 2) { //2 - View tasks
                     System.out.println(spacer);
                     System.out.println("#0 - " + stringTaskArray[0]);
                     System.out.println("#1 - " + stringTaskArray[1]);
@@ -48,35 +50,55 @@ public class Main {
                     System.out.println("#7 - " + stringTaskArray[7]);
                     System.out.println("#8 - " + stringTaskArray[8]);
                     System.out.println("#9 - " + stringTaskArray[9]);
-                    System.out.println(spacer + "\nYou can add up to 10 tasks!\n" + "You have " + currentTasks + "/10!\n" + spacer + "\n1) Manage tasks \n" + "2) View tasks\n" +"3) Exit");
-                } else if (menuInput == 3) {
-                    System.exit(0);
+                    System.out.println(spacer + "\nYou can add up to 10 tasks!\n" + "You have " + currentTasks + "/10!\n" + spacer + "\n1) Manage tasks \n" + "2) View tasks\n" + "3) Exit");
+                } else if (menuInput == 3) { //3 - Exit
+                    System.exit(0); //Exits the program.
                 } else {
-                    System.out.println("Incorrect input");
+                    System.out.println("Incorrect input"); //If there is a number like 4 or 5, it will say Incorrect input, but will crash if a word/letter is put in.
                 }
             }
 
             while (true) {
                 String secondMenuInput = sc.nextLine();
-                if (secondMenuInput.equals("1")) {
-                    System.out.println("Enter name of the new task:");
-                    stringTaskArray[currentTasks] = sc.nextLine();
-                    currentTasks++;
-                    break;
-                } else if (secondMenuInput.equals("2")) {
-                    System.out.println("Which task do you want to change:");
-                    int inputTaskChange = sc.nextInt();
-                    System.out.println("Enter name of the new task:");
-                    stringTaskArray[inputTaskChange] = sc.nextLine();
-                    break;
-                } else if (secondMenuInput.equals("3")) {
+                if (secondMenuInput.equals("1")) { //1 - New task
+                    if (currentTasks < 10) {
+                        System.out.println("Enter name of the new task:");
+                        stringTaskArray[currentTasks] = sc.nextLine();
+                        currentTasks++;
+                    } else {
+                        System.out.println("There are no more available tasks, use 'Update a task' instead");
+                    }
+                    break; // Breaks the while loop and goes to the other while loop.
+                } else if (secondMenuInput.equals("2")) { //2 - Modify tasks
+                    if (currentTasks > 0) {
+                        System.out.println("Which task do you want to change:");
+                        String inputTaskChange = sc.nextLine();
+                        if (Integer.parseInt(inputTaskChange) < 10) {
+                            System.out.println("Enter name of the new task:");
+                            stringTaskArray[Integer.parseInt(inputTaskChange)] = sc.nextLine();
+                        } else {
+                            System.out.println("Incorrect input");
+                        }
+                    } else {
+                        System.out.println("There is nothing to change");
+                    }
+                    break; // Breaks the while loop and goes to the other while loop.
+                } else if (secondMenuInput.equals("3")) { //3 - Destroy tasks
                     System.out.println("Which task do you want to destroy:");
                     int inputTaskDestroy = sc.nextInt();
-                    stringTaskArray[inputTaskDestroy] = "";
-                    currentTasks--;
-                    break;
-                } else if (secondMenuInput.equals("4")) {
-                    break;
+                    if (inputTaskDestroy < 10) {
+                        if (currentTasks > 0) {
+                            stringTaskArray[inputTaskDestroy] = "";
+                            currentTasks--;
+                        } else {
+                            System.out.println("Current Tasks number would go below zero.");
+                        }
+                    } else {
+                        System.out.println("Incorrect input");
+                    }
+                    break; // Breaks the while loop and goes to the other while loop.
+                } else if (secondMenuInput.equals("4")) { //4 - Back to main menu
+                    break; // Breaks the while loop and goes to the other while loop.
                 }
             }
         }
