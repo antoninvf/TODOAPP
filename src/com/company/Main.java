dea1f580ce39cebb81556d3bffafbf3ea187bf09
Antonín Vondrovic <antonin.vondrovic@educanet.cz>
2020-11-10 14:39:53 +0100

    code is now working, doing finishing touches

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,16 @@ public class Main {
         int currentTasks = 0;
 
         String[] stringTaskArray = new String[10];
+        stringTaskArray[0] = "";
+        stringTaskArray[1] = "";
+        stringTaskArray[2] = "";
+        stringTaskArray[3] = "";
+        stringTaskArray[4] = "";
+        stringTaskArray[5] = "";
+        stringTaskArray[6] = "";
+        stringTaskArray[7] = "";
+        stringTaskArray[8] = "";
+        stringTaskArray[9] = "";
 
         System.out.println(" _______ ____  _____   ____                         \n" +
                 "|__   __/ __ \\|  __ \\ / __ \\      /\\                \n" +
@@ -20,13 +30,26 @@ public class Main {
                 "                                       | |   | |    \n" +
                 "                                       |_|   |_|    ");
         while (true) {
-            System.out.println(spacer + "\nYou can add up to 10 tasks!\n" + "You have " + currentTasks + "/10!\n" + spacer + "\n1) Manage tasks \n" + "2) Exit");
+            System.out.println(spacer + "\nYou can add up to 10 tasks!\n" + "You have " + currentTasks + "/10!\n" + spacer + "\n1) Manage tasks \n" + "2) View tasks\n" +"3) Exit");
             while (true) {
                 int menuInput = sc.nextInt();
                 if (menuInput == 1) {
                     System.out.println(spacer + "\nTasks management\n" + spacer + "\n1) Create a new task\n2) Update a task\n3) Destroy a task\n4) Back to main menu");
                     break;
                 } else if (menuInput == 2) {
+                    System.out.println(spacer);
+                    System.out.println("#0 - " + stringTaskArray[0]);
+                    System.out.println("#1 - " + stringTaskArray[1]);
+                    System.out.println("#2 - " + stringTaskArray[2]);
+                    System.out.println("#3 - " + stringTaskArray[3]);
+                    System.out.println("#4 - " + stringTaskArray[4]);
+                    System.out.println("#5 - " + stringTaskArray[5]);
+                    System.out.println("#6 - " + stringTaskArray[6]);
+                    System.out.println("#7 - " + stringTaskArray[7]);
+                    System.out.println("#8 - " + stringTaskArray[8]);
+                    System.out.println("#9 - " + stringTaskArray[9]);
+                    System.out.println(spacer + "\nYou can add up to 10 tasks!\n" + "You have " + currentTasks + "/10!\n" + spacer + "\n1) Manage tasks \n" + "2) View tasks\n" +"3) Exit");
+                } else if (menuInput == 3) {
                     System.exit(0);
                 } else {
                     System.out.println("Incorrect input");
@@ -34,25 +57,26 @@ public class Main {
             }
 
             while (true) {
-                int secondMenuInput = sc.nextInt();
-                if (secondMenuInput == 1) {
+                String secondMenuInput = sc.nextLine();
+                if (secondMenuInput.equals("1")) {
                     System.out.println("Enter name of the new task:");
-                    stringTaskArray[1+currentTasks]= sc.nextLine();
+                    stringTaskArray[currentTasks] = sc.nextLine();
                     currentTasks++;
-                } else if (secondMenuInput == 2) {
-                    System.out.println("What task do you want to change:");
+                    break;
+                } else if (secondMenuInput.equals("2")) {
+                    System.out.println("Which task do you want to change:");
                     int inputTaskChange = sc.nextInt();
                     System.out.println("Enter name of the new task:");
                     stringTaskArray[inputTaskChange] = sc.nextLine();
-                } else if (secondMenuInput == 3) {
-                    System.out.println("What task do you want to destroy:");
+                    break;
+                } else if (secondMenuInput.equals("3")) {
+                    System.out.println("Which task do you want to destroy:");
                     int inputTaskDestroy = sc.nextInt();
                     stringTaskArray[inputTaskDestroy] = "";
                     currentTasks--;
-                } else if (secondMenuInput == 4) {
                     break;
-                } else {
-                    System.out.println("Incorrect input");
+                } else if (secondMenuInput.equals("4")) {
+                    break;
                 }
             }
         }
